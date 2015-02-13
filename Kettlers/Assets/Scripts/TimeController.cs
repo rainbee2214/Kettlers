8d729c5ed4cb4cb3ce20bc78cc719cd6c301b04d
rainbee2214 <rainbee2214@gmail.com>
2015-02-13 15:25:02 -0400

    Added a Time Controller.

---
+++ Kettlers/Assets/Scripts/TimeController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeController : MonoBehaviour
+{
+    public float timeScale = 1f;
+    public int startingHour = 8;
+    public bool tick;
+
+    float nextTickTime;
+    int currentSeconds, currentMinutes, currentHours;
+    int dayCount;
+
+    void Update()
+    {
+        if (tick && Time.time > nextTickTime) Tick();
+    }
+
+    public void Tick()
+    {
+        Debug.Log(ToString());
+        //tick = false;
+        currentSeconds++;
+        if (currentSeconds >= 60) TickMinutes();
+        nextTickTime = Time.time + timeScale;
+    }
+
+    void TickMinutes()
+    {
+        currentSeconds = 0;
+        currentMinutes++;
+        if (currentMinutes >= 60) TickHours();
+    }
+
+    void TickHours()
+    {
+        currentMinutes = 0;
+        currentHours++;
+        if (currentHours >= 12) currentHours = startingHour;
+    }
+
+    public override string ToString()
+    {
+        string time = ((currentHours+startingHour) < 10 ? "0" : "") + (currentHours + startingHour) + ":" +
+        (currentMinutes < 10 ? "0" : "") + currentMinutes + ":" +
+        (currentSeconds < 10 ? "0" : "") + currentSeconds;
+        return time;
+    }
+}
