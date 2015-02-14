d537c04e45c34495b30406f01711b60705a217c9
rainbee2214 <rainbee2214@gmail.com>
2015-02-14 18:50:22 -0400

    Added a ui placement class that dynamically reacts to the screen size to place the ui elements. This will help keep everything in a constant position no matter what.

---
+++ Kettlers/Assets/PlaceUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class PlaceUI : MonoBehaviour
+{
+    [Range(0f, 1f)]
+    public float widthPercentage, heightPercentage;
+
+    float screenWidth;
+    float screenHeight;
+    float lastWP, lastHP;
+    Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (screenHeight != Screen.height || screenWidth != Screen.width
+            || widthPercentage != lastWP || heightPercentage != lastHP) PositionUI();
+    }
+
+    void PositionUI()
+    {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+        lastWP = widthPercentage;
+        lastHP = heightPercentage;
+        text.rectTransform.position = new Vector3(screenWidth - screenWidth * (1 - widthPercentage), screenHeight - screenHeight * (1 - heightPercentage), 0);
+    }
+}
