3a4d23b40557fa46ad4c427e63a84dc9dd1b6051
rainbee2214 <rainbee2214@gmail.com>
2015-02-14 10:37:38 -0400

    Going to start some of the code over.

--- Kettlers/Assets/Scripts/UIController.cs
+++ Kettlers/Assets/Scripts/UIController.cs
@@ -5,9 +5,16 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     public Text text;
+    public Text dayCount;
+    public Text actionButtonText;
 
     void Update()
     {
         text.text = TimeController.timeController.GetTime();
+    }
+
+    public void SetActionButtonText(string buttonText)
+    {
+        actionButtonText.text = buttonText;
     }
 }
