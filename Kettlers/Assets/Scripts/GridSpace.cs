a9b5eae533531fd47f6a18473192882e24786758
rainbee2214 <rainbee2214@gmail.com>
2015-02-18 21:49:37 -0400

    Working on the grid and grid spaces.

---
+++ Kettlers/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+    public bool occupied;
+    public bool activated;
+
+    public Color activeColor;
+    public Color inactiveColor;
+
+    Image image;
+
+    void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+    public void SetUpSpace()
+    {
+        activated = true;
+        occupied = false;
+    }
+
+    public void TakeDownSpace()
+    {
+        activated = false;
+        occupied = false;
+    }
+
+    void Update()
+    {
+        Debug.Log("Gridspace is occupied: " + occupied + " and activated: " + activated);
+        if (activated) image.color = activeColor;
+        else image.color = inactiveColor;
+    }
+
+
+}
+
