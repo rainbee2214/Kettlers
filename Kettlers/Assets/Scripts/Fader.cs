3f8c0390e93071145f57eda434c76046003fc930
rainbee2214 <rainbee2214@gmail.com>
2015-03-04 21:06:55 -0400

    Working on the Fader object.

--- Kettlers/Assets/Scripts/Fader.cs
+++ Kettlers/Assets/Scripts/Fader.cs
@@ -4,10 +4,10 @@ using UnityEngine.UI;
 
 public class Fader : MonoBehaviour
 {
-    public Color start, target;
+    public Color start, target, transparent;
     public float speed = 0.01f;
     public bool on;
-
+    public bool activated;
     Image image;
 
     void Start()
@@ -15,16 +15,36 @@ public class Fader : MonoBehaviour
         image = GetComponent<Image>();
         image.color = start;
     }
+
     void Update()
     {
-        if (on)
+        if (activated)
         {
-            image.color = Color.Lerp(image.color, target, speed);
+            if (on)
+            {
+                image.color = Color.Lerp(image.color, target, speed*Time.deltaTime);
+                speed += 0.01f;
+                if (image.color == target)
+                {
+                    Debug.Log("Color target reached!");
+                    activated = false;
+                }
+            }
+            else
+            {
+                image.color = start;
+            }
         }
         else
         {
-            image.color = start;
-
+            image.color = transparent;
         }
     }
+
+    public void Activate()
+    {
+        activated = true;
+    }
+
+
 }
