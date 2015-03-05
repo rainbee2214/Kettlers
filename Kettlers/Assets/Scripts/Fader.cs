6281aef8d56963031f5148527e62f1ac666bb249
rainbee2214 <rainbee2214@gmail.com>
2015-03-05 16:44:41 -0400

    Working on getting the fader to be flexible, fading in and out on command.

--- Kettlers/Assets/Scripts/Fader.cs
+++ Kettlers/Assets/Scripts/Fader.cs
@@ -6,21 +6,26 @@ public class Fader : MonoBehaviour
 {
     public Color start, target, transparent;
     public float speed = 0.01f;
-    public bool on;
+    public bool fadeIn, fadeOut;
     public bool activated;
+    public bool reset;
+
+    float startSpeed;
     Image image;
 
     void Start()
     {
+        startSpeed = speed;
         image = GetComponent<Image>();
         image.color = start;
     }
 
     void Update()
     {
+        if (reset) Reset();
         if (activated)
         {
-            if (on)
+            if (fadeIn)
             {
                 image.color = Color.Lerp(image.color, target, speed*Time.deltaTime);
                 speed += 0.01f;
@@ -30,15 +35,22 @@ public class Fader : MonoBehaviour
                     activated = false;
                 }
             }
-            else
+            else if (fadeOut)
             {
-                image.color = start;
+                image.color = Color.Lerp(image.color, start, speed * Time.deltaTime);
+                speed += 0.01f;
+                if (image.color == target)
+                {
+                    Debug.Log("Color target reached!");
+                    activated = false;
+                }
             }
         }
         else
         {
             image.color = transparent;
         }
+        
     }
 
     public void Activate()
@@ -46,5 +58,12 @@ public class Fader : MonoBehaviour
         activated = true;
     }
 
+    public void Reset()
+    {
+        image.color = start;
+        activated = true;
+        reset = false;
+        speed = startSpeed;
+    }
 
 }
