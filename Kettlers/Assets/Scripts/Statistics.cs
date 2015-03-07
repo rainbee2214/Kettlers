8898eebd6436b37b34d8f978f8054604fc52d947
rainbee2214 <rainbee2214@gmail.com>
2015-03-07 16:18:13 -0400

    Statistics button shows a message popup.

---
+++ Kettlers/Assets/Scripts/Statistics.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Statistics : MonoBehaviour
+{
+    public GameObject messageBox;
+    public Button chartButton;
+
+    bool displayingMessage;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (displayingMessage && Input.GetButtonDown("CloseMessageBox")) CloseMessageBox();
+    }
+
+    public void ToggleMessageBox()
+    {
+        if (messageBox.activeSelf) CloseMessageBox();
+        else DisplayMessageBox();
+    }
+
+    public void DisplayMessageBox()
+    {
+        messageBox.gameObject.SetActive(true);
+        GameController.controller.PauseGame();
+        displayingMessage = true;
+    }
+
+    public void CloseMessageBox()
+    {
+        messageBox.gameObject.SetActive(false);
+        GameController.controller.UnPauseGame();
+        displayingMessage = false;
+    }
+}
