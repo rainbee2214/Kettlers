db3006928dd542b5fdfa4a2f51e3232b361c2a45
rainbee2214 <rainbee2214@gmail.com>
2015-03-15 19:43:18 -0300

    Adding chips.

---
+++ Kettlers/Assets/Scripts/Chips.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Chips : MonoBehaviour
+{
+    //Chips will have 2-5 ingredients, a string name, a base worth
+    public List<Resource.Type> ingredients;
+    string brandName;
+    public string BrandName
+    {
+        get { return brandName; }
+        set { brandName = value; }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public List<Resource.Type> GetIngredients()
+    {
+        return ingredients;
+    }
+
+    public void SetIngredients(List<Resource.Type> activeIngredients)
+    {
+        ingredients = activeIngredients;
+    }
+
+
+}
